03b9462f6bc4503abd80258e9c69a0f9fd969265
unknown <narikkodanathul@gmail.com>
2022-02-22 13:57:13 +0530

    component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Expense from "./components/expense";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expense></Expense>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/expense.js
@@ -0,0 +1,7 @@
+
+function Expense()
+{
+    return(<p>This is also visible!!!!!!!!!1</p>)
+    
+}
+export default Expense
