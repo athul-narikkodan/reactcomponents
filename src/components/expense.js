a69d2cbb942a1013aa2b18768f339051db59b7f0
athul-narikkodan <narikkodanathul@gmail.com>
2022-02-23 13:42:39 +0530

    first commit

--- src/components/expense.js
+++ src/components/expense.js
@@ -1,7 +1,15 @@
-
-function Expense()
-{
-    return(<p>This is also visible!!!!!!!!!1</p>)
-    
+import "./expenseItem.css";
+function Expense(props) {
+  return (
+    <div className="expense-item">
+      <div className="expense-item__description">{props.date.toISOString()}</div>
+      <div>
+        <div className="expense-item__description h2">
+          <h2>{props.title}</h2>
+        </div>
+        <div className="expense-item__price">Rs.{props.amount}</div>
+      </div>
+    </div>
+  );
 }
-export default Expense
+export default Expense;
